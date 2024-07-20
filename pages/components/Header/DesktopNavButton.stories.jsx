64b9de7a686eb33b8fe0d177ee7e3f81b5dbaa56
Kubota <74347995+KubotaDaiki@users.noreply.github.com>
2024-07-20 21:06:17 +0900

    feat: ヘッダーのstoryの追加 (#24)

---
+++ pages/components/Header/DesktopNavButton.stories.jsx
@@ -0,0 +1,18 @@
+import { DesktopNavButton } from "./DesktopNavButton";
+
+export default {
+  component: DesktopNavButton,
+  title: "Header/DesktopNavButton",
+  tags: ["autodocs"],
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    item: "ホーム",
+  },
+};
