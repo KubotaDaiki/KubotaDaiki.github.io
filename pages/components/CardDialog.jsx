0128c51aee2bb5e9e32733c2dba85f2cd6ca4618
Kubota <kubotadaiki0654@gmail.com>
2024-07-23 22:23:36 +0900

    feat: carddialogコンポーネントをtailwind+headless-uiへ移行

--- pages/components/CardDialog.jsx
+++ pages/components/CardDialog.jsx
@@ -1,41 +1,36 @@
-import CloseIcon from "@mui/icons-material/Close";
-import { useMediaQuery } from "@mui/material";
-import Dialog from "@mui/material/Dialog";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import IconButton from "@mui/material/IconButton";
-import { useTheme } from "@mui/material/styles";
+import { Dialog, DialogPanel, DialogBackdrop, DialogTitle } from '@headlessui/react'
+import { IoMdClose } from "react-icons/io";
+
 
 export default function CardDialogs({ title, open, handleClose, children }) {
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
   return (
-    <Dialog
-      onClose={handleClose}
-      open={open}
-      maxWidth="lg"
-      sx={{ p: 1 }}
-      fullScreen={fullScreen}
-    >
-      <DialogTitle>{title}</DialogTitle>
-      <IconButton
-        aria-label="close"
-        onClick={handleClose}
-        sx={{
-          position: "absolute",
-          right: 8,
-          top: 8,
-          color: (theme) => theme.palette.grey[500],
-        }}
-      >
-        <CloseIcon />
-      </IconButton>
-      <DialogContent
-        dividers
-        sx={{ paddingX: { md: "50px" }, paddingY: "30px" }}
-      >
-        {children}
-      </DialogContent>
+    <Dialog open={open} as="div" className="relative z-[500] duration-200 ease-out data-[closed]:opacity-0" onClose={handleClose} transition>
+      <div className="
+        fixed inset-0 w-screen p-5
+        flex items-center justify-center
+        bg-[#000000] bg-opacity-50
+      ">
+        <div className="  ">
+          <DialogPanel
+            className="relative w-full max-w-6xl rounded bg-[#ffffff] overflow-y-auto max-h-screen"
+          >
+            <DialogTitle as="h3" className="text-xl text-primary px-6 py-4">
+              {title}
+            </DialogTitle>
+            <button onClick={handleClose} className='
+              absolute top-3 right-3
+              hover:bg-[#e8e8e8] transition duration-200
+              p-2 rounded-full
+              '>
+              <IoMdClose size={24} />
+            </button>
+            <hr className='border-[#e6e6e6]' />
+            <div className='p-10'>
+              {children}
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
     </Dialog>
-  );
+  )
 }
