7492c601348c79ffb600d8f2d9c2d6f4af8c41df
Kubota <74347995+KubotaDaiki@users.noreply.github.com>
2024-07-23 17:23:11 +0900

    feat: 実績セクションにおけるmuiからtailwind+headless-uiへの移行 (#53)

--- pages/components/Achievements/AchievementListItem.jsx
+++ pages/components/Achievements/AchievementListItem.jsx
@@ -1,19 +1,15 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Image from "next/image";
 
 export function AchievementListItem({ achievement }) {
   return (
-    <>
-      <Box sx={{ mr: 1 }}>
-        <Image
-          src={`https:${achievement.fields.icon.fields.file.url}`}
-          width={20}
-          height={20}
-          alt="achievement"
-        ></Image>
-      </Box>
-      <Typography variant="p">{achievement.fields.name}</Typography>
-    </>
+    <div className="flex items-center gap-3">
+      <Image
+        src={`https:${achievement.fields.icon.fields.file.url}`}
+        width={20}
+        height={20}
+        alt="achievement"
+      ></Image>
+      <p>{achievement.fields.name}</p>
+    </div>
   );
 }
