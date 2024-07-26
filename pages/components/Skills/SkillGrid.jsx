35dfc57461c73bafdf2796bb457c9c671585cffa
Kubota <74347995+KubotaDaiki@users.noreply.github.com>
2024-07-26 23:01:45 +0900

    feat: skillgridのclassnameのgrid-itemを削除 (#73)

--- pages/components/Skills/SkillGrid.jsx
+++ pages/components/Skills/SkillGrid.jsx
@@ -4,7 +4,7 @@ export function SkillGrid({ skills }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-3xl">
       {skills.fields.skills.map((skill) => (
-        <div className="grid-item" key={skill.sys.id}>
+        <div key={skill.sys.id}>
           <SkillGridItem skill={skill}></SkillGridItem>
         </div>
       ))}
